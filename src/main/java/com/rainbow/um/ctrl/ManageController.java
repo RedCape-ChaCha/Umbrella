9be32d39fec79a1f013c2e9bfa05861d1efdd874
LeeGyeoRe <furion94@hanmail.net>
2019-11-27 09:51:25 +0900

    Manage Service 테스트 완료 및 SMS 전송 테스트 완료

--- src/main/java/com/rainbow/um/ctrl/ManageController.java
+++ src/main/java/com/rainbow/um/ctrl/ManageController.java
@@ -4,11 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.rainbow.um.model.IManageService;
 
+@Controller
 public class ManageController {
 	
 	@Autowired
@@ -19,7 +21,7 @@ public class ManageController {
 		return "ManageTest";
 	}
 	
-	@RequestMapping(value = "/loan.login", method = RequestMethod.GET)
+	@RequestMapping(value = "/loan.do", method = RequestMethod.GET)
 	public String loan() {
 		Map<String,String> map = new HashMap<String, String>();
 		map.put("user_number", "2");
@@ -28,5 +30,58 @@ public class ManageController {
 		System.out.println(i);
 		return "ManageTest";
 	}
-
+	
+	@RequestMapping(value = "/apply.do", method = RequestMethod.GET)
+	public String apply() {
+		Map<String,String> map = new HashMap<String, String>();
+		map.put("user_number", "2");
+		map.put("book_cseq","3");
+		System.out.println("웹 대출 신청 성공 여부");
+		System.out.println(manage.applyInsert(map));
+		return "ManageTest";
+	}
+	
+	@RequestMapping(value = "/applyCancle.do", method = RequestMethod.GET)
+	public String applyCancle() {
+		Map<String,String> map = new HashMap<String, String>();
+		map.put("user_number", "2");
+		map.put("book_cseq","3");
+		System.out.println("웹 대출 취소 성공 여부");
+		System.out.println(manage.applyUpdate(map));
+		return "ManageTest";
+	}
+	
+	@RequestMapping(value = "/return.do", method = RequestMethod.GET)
+	public String return3() {
+		Map<String,String> map = new HashMap<String, String>();
+		map.put("user_number", "2");
+		map.put("book_aseq", "2");
+		System.out.println(manage.returnBook(map));
+		return "ManageTest";
+	}
+	
+	@RequestMapping(value = "/resv.do", method = RequestMethod.GET)
+	public String resv() {
+		Map<String,String> map = new HashMap<String, String>();
+		map.put("user_number", "2");
+		map.put("book_cseq", "2");
+		System.out.println(manage.normalResvInsert(map));
+		return "ManageTest";
+	}
+	
+	@RequestMapping(value = "/milgResv.do", method = RequestMethod.GET)
+	public String milgResv() {
+		Map<String,String> map = new HashMap<String, String>();
+		map.put("user_number", "2");
+		map.put("book_cseq", "2");
+		System.out.println(manage.milgResvInsert(map));
+		return "ManageTest";
+	}
+	
+	@RequestMapping(value = "/cancleResv.do", method = RequestMethod.GET)
+	public String cancleResv() {
+		System.out.println("마일리지 예약 취소");
+		System.out.println(manage.cancleResv("3"));
+		return "ManageTest";
+	}
 }
