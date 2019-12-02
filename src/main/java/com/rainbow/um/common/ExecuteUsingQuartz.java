da25c8c2a6235cd756d6ee81342a3c7f4190be6b
ASUS <ASUS@DESKTOP-44OB82G>
2019-12-02 16:27:54 +0900

    배치 실행 메소드 수정

--- src/main/java/com/rainbow/um/common/ExecuteUsingQuartz.java
+++ src/main/java/com/rainbow/um/common/ExecuteUsingQuartz.java
@@ -14,16 +14,6 @@ public class ExecuteUsingQuartz {
 	
 	
 	public void sendMessage() throws Exception {
-		String setFrom = "rkd1rjs2@gmail.com"; //보내는 사람
-		String toMail = "rkd1rjs2@naver.com";
-		String title ="휴면계정 정지 안내";
-		String content = "배치테스트";
-		MimeMessage message = mailSender.createMimeMessage(); //메일 보내는 객체
-		MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
-		messageHelper.setFrom(setFrom); //보내는 사람 주소, 생략하거나 입력안하면 작동 되지 않음
-		messageHelper.setTo(toMail); //받는 사람의 주소
-		messageHelper.setSubject(title); //제목 , 생략가능
-		messageHelper.setText(content, true); //messageHelper.setText(content); //전송 내용 //setText(content,boolean(default는 false,html사용여부));
-		mailSender.send(message);
+
 	}
 }
