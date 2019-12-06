d4d4c0c95a00e5ff133a0d3ca09428284dd3e80a
Gyeore Lee <33143327+furion94@users.noreply.github.com>
2019-12-06 18:24:10 +0900

    파일업로드 썸네일 추가, 토스 불필요한 코드 수정

--- src/main/java/com/rainbow/um/common/S3FileUpload.java
+++ src/main/java/com/rainbow/um/common/S3FileUpload.java
@@ -1,6 +1,15 @@
 package com.rainbow.um.common;
 
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
 
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.AWSCredentials;
@@ -8,34 +17,80 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
 public class S3FileUpload {
-	private static String BUCKET_NAME;
-    private AmazonS3 amazonS3;
+	private AmazonS3 amazonS3;
+	private String bucket_name;
+	private String access_key;
+	private String secret_key;
  
     public S3FileUpload() {
     }
     
-    public S3FileUpload(String access_key, String secret_key, String bucket_name) {
-    	this.BUCKET_NAME = bucket_name;
+    public void makeS3() {
     	AWSCredentials awsCredentials = new BasicAWSCredentials(access_key, secret_key);
     	amazonS3 = new AmazonS3Client(awsCredentials);
     }
+    
+    private void normalUpload(String path, File file) {
+    	PutObjectRequest putObjectRequest =
+                new PutObjectRequest(bucket_name + "/" + path, file.getName(), file);
+        putObjectRequest.setCannedAcl(CannedAccessControlList.PublicRead); // file permission
+        amazonS3.putObject(putObjectRequest);
+    }
  
     public void uploadFile(String path, File file) {
-        if (amazonS3 != null) {
-            try {
-                PutObjectRequest putObjectRequest =
-                        new PutObjectRequest(BUCKET_NAME + "/" + path, file.getName(), file);
-                putObjectRequest.setCannedAcl(CannedAccessControlList.PublicRead); // file permission
-                amazonS3.putObject(putObjectRequest); // upload file
- 
-            } catch (AmazonServiceException ase) {
-                ase.printStackTrace();
-            } finally {
-                amazonS3 = null;
-            }
+    	makeS3();
+        try {
+        	normalUpload(path, file);
+        } catch (AmazonServiceException ase) {
+            ase.printStackTrace();
+        } finally {
+            amazonS3 = null;
         }
     }
+    
+    public void uploadThumbnail(String path, File file, String fileType) {
+    	makeS3();
+    	try {
+        	normalUpload(path, file);
+        	Image image = ImageIO.read(file);
+			int w = 180;
+			int h = 600;
+			Image resize = image.getScaledInstance(w, h, Image.SCALE_SMOOTH);
+			BufferedImage newImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+            Graphics g = newImage.getGraphics();
+            g.drawImage(resize, 0, 0, null);
+            g.dispose();
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            ImageIO.write(newImage, fileType, os);
+            byte[] buffer = os.toByteArray();
+            InputStream is = new ByteArrayInputStream(buffer);
+            ObjectMetadata meta = new ObjectMetadata();
+            meta.setContentLength(buffer.length);
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucket_name + "/ThumbnailImg", file.getName(), is, meta);
+            putObjectRequest.setCannedAcl(CannedAccessControlList.PublicRead);
+            amazonS3.putObject(putObjectRequest);
+        } catch (AmazonServiceException | IOException ase) {
+            ase.printStackTrace();
+        } finally {
+            amazonS3 = null;
+        }
+    }
+
+	public void setBucket_name(String bucket_name) {
+		this.bucket_name = bucket_name;
+	}
+
+	public void setAccess_key(String access_key) {
+		this.access_key = access_key;
+	}
+
+	public void setSecret_key(String secret_key) {
+		this.secret_key = secret_key;
+	}
+    
+    
 }
