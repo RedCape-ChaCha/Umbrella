afe7297b8d7ff349f6e9100afe61b36baa3fdb02
cvcv0802 <55830162+cvcv0802@users.noreply.github.com>
2019-12-06 11:39:52 +0900

    최신화

--- src/main/java/com/rainbow/um/ctrl/ManageController.java
+++ src/main/java/com/rainbow/um/ctrl/ManageController.java
@@ -1,5 +1,11 @@
 package com.rainbow.um.ctrl;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,7 +15,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.rainbow.um.common.OtpWAS;
 import com.rainbow.um.common.TossAPI;
 import com.rainbow.um.model.IManageService;
 
@@ -20,6 +28,8 @@ public class ManageController {
 	private IManageService manage;
 	@Autowired
 	private TossAPI toss;
+	@Autowired
+	private OtpWAS otp;
 	
 	@RequestMapping(value = "/testManage.do", method = RequestMethod.GET)
 	public String home() {
@@ -132,23 +142,29 @@ public class ManageController {
 		System.out.println(toss.tossCancle(payToken).toString());
 		return "Test/ManageTest";
 	}
+	
 	@RequestMapping(value = "/timeChk.do", method = RequestMethod.GET)
 	public String timeChk(String time, String code) throws Exception {
 		long tm = Long.parseLong(time);
 		System.out.println(otp.vaildate(code, "01055231605", tm));
 		return "Test/ManageTest";
 	}
+	
 	@RequestMapping(value = "/doAjax.do", method = RequestMethod.POST)
 	@ResponseBody
 	public String doAjax(String time, String phone) throws Exception {
 		try{
 			String sUrl = "http://localhost:8087/Umbrella_Batch/send.do?phone="+phone+"&time="+time;
-			URL callUrl = new URL(sUrl);
+	
+			URL callUrl = new URL(sUrl);   
 			URLConnection urlConn = callUrl.openConnection();
+			
 			InputStream is = urlConn.getInputStream();
 			InputStreamReader isr = new InputStreamReader(is);
 			BufferedReader br = new BufferedReader(isr);
+			
 			String buf = null;
+
 			while(true){
 				buf = br.readLine();
 				if(buf == null) break;
